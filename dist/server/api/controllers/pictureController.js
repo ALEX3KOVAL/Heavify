5f895fbe78ca37b2740ead5f8f927d41fb3ee862
Alexandr <koval3nich@yandex.ru>
2022-10-31 05:56:01 +0500

    fix some bugs

--- dist/server/api/controllers/pictureController.js
+++ dist/server/api/controllers/pictureController.js
@@ -5,6 +5,7 @@ const getPicturesGroupByNames = async (req, res, next) => {
     if (pageName && componentType) {
         readdir(`../../assets/images/${pageName}_page/${componentType}/${componentName}`, (err, files) => {
             if (err) {
+                console.log(err.message);
                 return next(ApiError.identify(err));
             }
             return res.json({ filesNames: files.sort((a, b) => Number(a.slice(a.lastIndexOf("_") + 1, a.lastIndexOf("."))) > Number(b.slice(b.lastIndexOf("_") + 1, b.lastIndexOf("."))) ? 1 : -1) });
