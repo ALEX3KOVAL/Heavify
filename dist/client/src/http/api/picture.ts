a767f51e4d9926c55b6bdcbdc7999a1a6e975a80
Alexandr <koval3nich@yandex.ru>
2022-11-07 05:11:22 +0500

    fix picture-HTTP-API

--- dist/client/src/http/api/picture.ts
+++ dist/client/src/http/api/picture.ts
@@ -2,6 +2,6 @@ import PictureService from "@/services/picture";
 
 const getPicturesGroupByNames = async (pageName: string, componentType: string, componentName = ""): Promise<string[]> => {
     const response = await PictureService.getPicturesGroupByNames(pageName, componentType, componentName);
-    return response.data;
+    return response.data.filesNames;
 }
 export default getPicturesGroupByNames;
