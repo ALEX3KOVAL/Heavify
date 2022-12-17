fb71cf130858682a3646ee21be7c11ad466cdd7f
Alexandr <koval3nich@yandex.ru>
2022-12-17 05:07:00 +0300

    New image

---
+++ dist/client/src/layouts/HomeLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <section
+    :style="`background-image: url('${this.API_URL}/home_page/background/${this.backgroundImagePath}')`"
+  >
+    <slot />
+  </section>
+</template>
+
+<script>
+import {getPicturesGroupByNames} from "../services/picture.js";
+
+export default {
+  name: "HomeLayout",
+  async created() {
+    this.API_URL = process.env.VUE_APP_API_URL;
+    await getPicturesGroupByNames("home", "background")
+        .then(fileName => this.backgroundImagePath = fileName);
+  }
+}
+</script>
