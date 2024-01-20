58b1c6e4e25fb8bc063547356dc4e91db45109c5
Alexandr Kovalenko <destructsymp@gmail.com>
2024-01-20 15:59:31 +0300

    Снёс.

--- src/server/api/controllers/controllers.ts
+++
@@ -1,7 +0,0 @@
-export {AlbumController} from "./albumController";
-export {AuthorController} from "./authorController";
-export {GenreController} from "./genreController";
-export {PictureController} from "./pictureController";
-export {SelectionController} from "./selectionController";
-export {SongController} from "./songController";
-export {UserController} from "./userController";
--- src/server/dtos/user.ts
+++
@@ -1,7 +0,0 @@
-function UserDTO(model: any) {
-    this.id = model.id;
-    this.email = model.email;
-    this.isActivated = model.isActivated;
-}
-
-export default UserDTO;
