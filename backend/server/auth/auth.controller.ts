6daa4304559c8b847ddfb16d88c5cb5c76f212c4
Alexandr Kovalenko <destructsymp@gmail.com>
2024-03-31 18:19:04 +0300

    feature/registration: JsDocs.

--- backend/server/auth/auth.controller.ts
+++ backend/server/auth/auth.controller.ts
@@ -7,24 +7,39 @@ import { LogoutDTO } from './common/dto/logout.dto';
 import { PublicRoute } from './common/core/decorator/public-route';
 import { Token } from './common/vo/token';
 
+/**
+ * Эндпоинты регистрации, аутентификации и деавторизации клиента
+ */
 @Controller("auth")
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
+  /**
+   * Эндпоинт регистрации клиента
+   *
+   * @param registerDto ДТО регистрации
+   */
   @Post("register")
   @PublicRoute()
   async register(@BodyRegisterDTO() registerDto: RegisterDTO): Promise<{ access_token: Token, auth_method: string }> {
     return await this.authService.register(registerDto).getOrThrow();
   }
 
+  /**
+   * Эндпоинт аутентификации клиента
+   *
+   * @param loginDto ДТО аутентификации клиента
+   */
   @Post("login")
   @PublicRoute()
   async login(@BodyLoginDTO() loginDto: LoginDto): Promise<{ access_token: Token, auth_method: string }> {
     return await this.authService.login(loginDto).getOrThrow();
   }
 
+  /**
+   * Эндпоинт деавторизации клиента
+   * @param logoutDto
+   */
   @Post("logout")
   async logout(@BodyLogoutDTO() logoutDto: LogoutDTO): Promise<void> {
     return await this.authService.logout(logoutDto).getOrThrow();
