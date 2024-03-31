92c0aae2ca0df64e371d956076d0fc103d7cd047
Alexandr Kovalenko <destructsymp@gmail.com>
2024-03-31 18:21:41 +0300

    feature/registration: Форматирование.

--- backend/server/auth/by-bearer-token/bearer-token.guard.ts
+++ backend/server/auth/by-bearer-token/bearer-token.guard.ts
@@ -27,10 +27,10 @@ export class BearerTokenAuthGuard extends AuthGuard("bearer") {
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic: boolean = this.reflector
-      .getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+      .getAllAndOverride<boolean>(
+          IS_PUBLIC_KEY,
+          [context.getHandler(), context.getClass(),]
+      );
 
     if (isPublic) {
       return true;
